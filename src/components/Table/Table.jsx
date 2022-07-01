4533197bc5917f9ba6a32d1663a6929eac7d9a69
sadath <sadathshariff65@gmail.com>
2022-07-01 11:55:21 +0530

    Feat: Table Component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,97 @@
+import "./Table.css";
+import { Editable } from "./Editable";
+import { useAdmin } from "../../context/adminContext";
+import { getAllRowsSelected } from "../../context/utils";
+export const Table = () => {
+  const { adminDispatch, users, adminState } = useAdmin();
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input
+                type="checkbox"
+                checked={getAllRowsSelected(adminState)}
+                onChange={(e) =>
+                  adminDispatch({
+                    type: "SELECT_ALL",
+                    payload: {
+                      checked: e.target.checked,
+                      filteredUsers: users,
+                    },
+                  })
+                }
+              />
+            </th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length !== 0 ? (
+            <>
+              {users?.map((user) => (
+                <tr
+                  key={user.id}
+                  className={`${user.isChecked ? "selected-row" : ""}`}
+                >
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={user.isChecked}
+                      onChange={(e) => {
+                        adminDispatch({
+                          type: "SELECTED_ROW",
+                          payload: { checked: e.target.checked, id: user.id },
+                        });
+                      }}
+                    />
+                  </td>
+                  {user.isEdit ? (
+                    <Editable userData={user} />
+                  ) : (
+                    <>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.role}</td>
+                      <td>
+                        <button
+                          className="action-btns"
+                          onClick={() =>
+                            adminDispatch({
+                              type: "EDIT_USER",
+                              payload: user.id,
+                            })
+                          }
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="action-btns delete-btn"
+                          onClick={() =>
+                            adminDispatch({
+                              type: "DELETE_USER",
+                              payload: user.id,
+                            })
+                          }
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+            </>
+          ) : (
+            <span>No Users</span>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
