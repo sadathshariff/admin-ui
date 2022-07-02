316a448885981451bc4b7968a456f2f8f62d6200
sadath <sadathshariff65@gmail.com>
2022-07-02 16:38:32 +0530

    style: fixed  table for mobile view

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ function App() {
               Delete Selected
             </button>
           </nav>
-          <Table />
+          <div className="table-container">
+            <Table />
+          </div>
         </div>
         <Pagination />
       </div>
